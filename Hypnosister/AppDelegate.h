1d5f9bfd774ec1070f88ae210224e568e316f474
sashablokhin <sashablokhindev@gmail.com>
2016-05-26 16:47:14 +0300

    Create project

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Alexander Blokhin on 26.05.16.
+//  Copyright © 2016 Alexander Blokhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
