8f9a314bdf37501f82b2cbadeb7e3ccaaa8fe0ec
sashablokhin <sashablokhindev@gmail.com>
2016-05-30 12:15:51 +0300

    Added scroll view

--- Hypnosister/ViewController.m
+++ Hypnosister/ViewController.m
@@ -18,11 +18,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    HypnosisView *view = [[HypnosisView alloc] initWithFrame:self.view.bounds];
+    //HypnosisView *view = [[HypnosisView alloc] initWithFrame:self.view.bounds];
+    //[self.view addSubview:view];
     
-    [self.view addSubview:view];
+    CGRect screenRect = self.view.bounds;
+    
+    // Создание UIScrollView таким образом, чтобы размер окна соответствовал размеру области прокрутки
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    [self.view addSubview:scrollView];
+    
+    // Создание HypnosisView с фреймом, который в два раза больше размера экрана
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2.0;
+    bigRect.size.height *= 2.0;
+    
+    HypnosisView *view = [[HypnosisView alloc] initWithFrame:bigRect];
+    
+    [scrollView addSubview:view];
+    [scrollView setContentSize:bigRect.size];
     
     [view becomeFirstResponder]; // что бы наш вью принимал события
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
